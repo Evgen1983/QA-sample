54f0b2f7c5824ee10d07bf537e84e3b66e7c14b9
Evgen1983 <ebaranov1983@gmail.com>
2016-05-16 01:05:34 +0300

    add green questions test

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :question do
-    title "MyString"
-    body "MyText"
+    title "MyString is not too short"
+    body "MyText is not too short, because it contains more than 30 characters"
   end
 end
