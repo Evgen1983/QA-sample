284ff694047cb9450c8bcff9baef80b93798fbfa
Evgen1983 <ebaranov1983@gmail.com>
2016-05-16 12:25:45 +0300

    add show&new&edit and refactoring

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,20 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
 	describe 'GET #index' do
+	   let(:questions) { create_list(:question, 2) }
+	  before { get :index }
 	  it 'populates an array of all questions' do
-	  	question1 = FactoryGirl.create(:question)
-	  	question2 = FactoryGirl.create(:question)
-
-	  	get :index
-	  	expect(assigns(:questions)).to match_array([question1,question2])
-
+	  	expect(assigns(:questions)).to match_array(questions)
 	  end
 	  it 'renders index view' do
-	  	get :index
 	  	expect(response).to render_template :index
 	  end
-
 	end
 
+	describe 'GET #show'do
+	  before { get :show, id: question }
+	  it 'assings the requested question to @question' do
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'renders show view' do
+        expect(response).to render_template :show 
+      end
+    end
+
+    describe 'GET #new' do
+    before { get :new }
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, id: question }
+    it 'assings the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
 end
