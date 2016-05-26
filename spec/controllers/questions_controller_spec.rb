0d7bf306f65fedd97558c6793234ec6a48a9397f
Evgen1983 <ebaranov1983@gmail.com>
2016-05-26 18:18:32 +0300

    add story - any users can browse question index

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -99,8 +99,8 @@ RSpec.describe QuestionsController, type: :controller do
 
       it 'does not change question attributes' do
         question.reload
-        expect(question.title).to eq 'MyString is not too short'
-        expect(question.body).to eq 'MyText is not too short, because it contains more than 30 characters'
+        expect(question.title).to eq question[:title]
+        expect(question.body).to eq question[:body]
       end
 
       it 're-renders edit view' do
