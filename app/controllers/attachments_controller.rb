f3b2753e3cd3e39cbbeebe8050239abbb6a88da3
Evgen1983 <ebaranov1983@gmail.com>
2016-06-29 12:28:28 +0300

    add delete files

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,2 +1,14 @@
 class AttachmentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_attachment, only: [:destroy]
+
+  def destroy
+    @attachment.destroy if author_of?(@attachment.attachable)
+  end
+
+  private
+
+  def load_attachment
+    @attachment = Attachment.find(params[:id])
+end
 end
--- spec/acceptance/Question/add_files_to_question_spec.rb
+++ spec/acceptance/Question/add_files_to_question_spec.rb
@@ -6,42 +6,39 @@ feature 'Add files to question', %q{
   I'd like to be able to attach files
 } do
 
-  given(:user) { create(:user) }
+  given(:users) { create_pair(:user) }
+ 
+
   
   background do
-    sign_in(user)
+    sign_in(users[0])
     visit questions_path
-  end
-
-  scenario 'User adds file when asks and then edit question', js: true do
     click_on 'Ask Question'
     fill_in 'Title', with: 'Test question one'
-    fill_in 'Body', with: 'text text text text text text text'  
-     
+    fill_in 'Body', with: 'text text text text text text text'
     within all(:css, ".nested-fields").first do
       attach_file 'File', "#{Rails.root}/public/robots.txt"
     end
-    
+  end
+
+  scenario 'Author adds more files when asks and then edit question', js: true do
     click_on 'Add Attachment'
     
     within all(:css, ".nested-fields").last do
       attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
     end
-    
     click_on 'Create'
-    
     expect(page).to have_link 'robots.txt', href: '/uploads/attachment/file/1/robots.txt'
     expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
 
     click_on 'Edit Question'
 
     within '.question' do
-    
       click_on 'Add Attachment'
 
       within all(:css, ".nested-fields").first do
         attach_file 'File', "#{Rails.root}/public/favicon.ico"
-      end
+       end
     
       click_on 'Add Attachment'
     
@@ -56,5 +53,43 @@ feature 'Add files to question', %q{
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/4/spec_helper.rb'
   end
 
+  scenario 'Author can delete files', js: true do
+    
+    click_on 'Create'
+    
+    within ".question"do
+      expect(page).to have_link 'robots.txt', href: '/uploads/attachment/file/1/robots.txt'
+      click_on 'Delete File'
+      expect(page).to_not have_link 'robots.txt', href: '/uploads/attachment/file/1/robots.txt'
+    end
+  end
+  
+  context "Not Author" do
+    before do  
+      click_on 'Create'
+      click_on 'Sign out'
+    end
+    scenario "and authentificated user can't delete files", js: true do
+      sign_in(users[1])
+      visit questions_path
+      click_on 'Test question one'
+    
+      within ".question"do
+        expect(page).to have_link 'robots.txt', href: '/uploads/attachment/file/1/robots.txt'
+        expect(page).to_not have_link 'Delete File'
+      end
+    end
+    
+    scenario " and not authentificated user can't delete files", js: true do
+      visit questions_path
+      click_on 'Test question one'
+    
+      within ".question"do
+        expect(page).to have_link 'robots.txt', href: '/uploads/attachment/file/1/robots.txt'
+        expect(page).to_not have_link 'Delete File'
+      end
+    end
+
+  end  
 
 end
