20f46b962453881e2225a9bf5cd1b933945b78c8
Evgen1983 <ebaranov1983@gmail.com>
2016-08-20 17:09:53 +0300

    some style changes

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,9 @@
 class Search < ActiveRecord::Base
+  
   TYPES = %w(all questions answers comments users).freeze
-
+ 
   def self.find(request, type)
     query = Riddle::Query.escape(request)
-
     if type == 'all'
       ThinkingSphinx.search query
     else
