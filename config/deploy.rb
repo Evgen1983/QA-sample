fbda86d1544f250fb05e9b1079d34821642ff3fa
Evgen1983 <ebaranov1983@gmail.com>
2016-08-23 01:44:04 +0300

    final deploy

---
+++ config/deploy.rb
@@ -0,0 +1,82 @@
+# config valid only for current version of Capistrano
+lock '3.6.0'
+
+set :application, 'QA-sample'
+set :repo_url, 'git@github.com:Evgen1983/QA-sample.git'
+
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/QA-sample"
+set :deploy_user, 'deployer'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/private_pub.yml', 'config/private_pub_thin.yml', '.env'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+       execute :touch, release_path.join('tmp/restart.txt')
+      
+    end
+  end
+
+  after :publishing, :restart
+end
+
+namespace :private_pub do
+  desc 'Start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C ~/QA-sample/shared/config/private_pub_thin.yml start"
+        end
+      end
+    end
+  end
+
+  desc 'Stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C ~/QA-sample/shared/config/private_pub_thin.yml stop"
+        end
+      end
+    end
+  end
+
+  desc 'Restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C ~/QA-sample/shared/config/private_pub_thin.yml restart"
+        end
+      end
+    end
+  end
+end
+
+after 'deploy:restart', 'private_pub:restart'
