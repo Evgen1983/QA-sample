131477269c40d1c41edb2bdbba1d4fd7207fb405
Evgen1983 <ebaranov1983@gmail.com>
2016-08-25 13:21:58 +0300

    add cashe for question ans answer

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module QASample
   class Application < Rails::Application
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
-
+     config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
