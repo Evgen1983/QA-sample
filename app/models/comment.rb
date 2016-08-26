4496b19b79bd16665ac1598d8928d3d158eb5acc
Evgen1983 <ebaranov1983@gmail.com>
2016-08-26 14:33:38 +0300

    add cache for unregistred users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true, touch: true
   validates :content, presence: true, length: { maximum: 1000 }
+  default_scope { order(created_at: :asc) }
 end
